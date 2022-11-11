87d54a48ca67740cf769d95a1f2512226bccc6f8
Luis Martínez <lemartinezm@unitru.edu.pe>
2022-11-11 16:14:04 -0500

    add main server app

---
+++ src/main.ts
@@ -0,0 +1,35 @@
+import server from "./server";
+import ContactsRouter from "./presentation/routers/contactRouter";
+import { GetAllContacts } from "./domain/useCases/contact/getAllContacts";
+import { CreateContact } from "./domain/useCases/contact/createContact";
+import { ContactRepositoryImpl } from "./domain/repositories/contactRepository";
+import { MongoClient } from "mongodb";
+import { Database } from "./data/interfaces/dataSources/database";
+import { MongoDBContactDataSource } from "./data/dataSources/mongodb/mongodbContactDataSource";
+
+(async () => {
+  const client: MongoClient = new MongoClient(
+    "mongodb://localhost:27017/contacts"
+  );
+  await client.connect();
+  const db = client.db("CONTACTS_DB");
+
+  const contactDatabase: Database = {
+    find: (query) => db.collection("contacts").find(query).toArray(),
+    insertOne: (doc) => db.collection("contacts").insertOne(doc),
+  };
+
+  const contactMiddleware = ContactsRouter(
+    new GetAllContacts(
+      new ContactRepositoryImpl(new MongoDBContactDataSource(contactDatabase))
+    ),
+    new CreateContact(
+      new ContactRepositoryImpl(new MongoDBContactDataSource(contactDatabase))
+    )
+  );
+
+  server.use("/contact", contactMiddleware);
+  server.listen(4000, () => {
+    console.log("Running on server");
+  });
+})();
